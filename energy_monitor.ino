3fdaf4b60c74ac326f09af68333d0700412b5979
A Tiwari <54511186+drtiwari@users.noreply.github.com>
2023-07-15 11:13:06 +0200

    Update energy_monitor.ino

--- energy_monitor.ino
+++ energy_monitor.ino
@@ -1,26 +1,26 @@
 // EmonLibrary examples openenergymonitor.org, Licence GNU GPL V3
 
-#include "EmonLib.h"             // Include Emon Library
+#include "EmonLib.h"                    // Include Emon Library
 
 #define VOLT_CAL 333.1
 #define CURRENT_CAL 61.6
 
-EnergyMonitor emon1;             // Create an instance
+EnergyMonitor emon1;                    // Create an instance
 
 void setup()
 {  
   Serial.begin(9600);
   
-  emon1.voltage(1, VOLT_CAL, 1.7);  // Voltage: input pin, calibration, phase_shift
-  emon1.current(0, CURRENT_CAL);       // Current: input pin, calibration.
+  emon1.voltage(1, VOLT_CAL, 1.7);      // Voltage: input pin, calibration, phase_shift
+  emon1.current(0, CURRENT_CAL);        // Current: input pin, calibration.
 }
 
 void loop()
 {
-  emon1.calcVI(20,2000);                          // Calculate all. No.of half wavelengths (crossings), time-out
+  emon1.calcVI(20,2000);                 // Calculate all. No.of half wavelengths (crossings), time-out
 
-  float currentDraw     = emon1.Irms;             //extract Irms into Variable
-  float supplyVoltage   = emon1.Vrms;             //extract Vrms into Variable
+  float currentDraw     = emon1.Irms;    // Extract Irms into Variable
+  float supplyVoltage   = emon1.Vrms;    // Extract Vrms into Variable
 
   Serial.print("Voltage: ");
   Serial.println(supplyVoltage);
